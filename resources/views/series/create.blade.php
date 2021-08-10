160595c169f720bc80cf2e3068ded157c9dfaf9b
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-08-09 22:10:36 -0300

    Aula 4

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('cabecalho', 'Adicionar Séries')
+
+@section('conteudo')
+    <form action="post">
+        <div class="form-group">
+            <label for="nome">Nome</label>
+            <input type="text" class="form-control" name="nome" id="nome">
+        </div>
+
+        <button class="btn btn-primary">Adicionar</button>
+    </form>
+@endsection
